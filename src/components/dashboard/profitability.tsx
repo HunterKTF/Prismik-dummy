49ba0bc768c05ad6c89833f4979df9aa73444b2e
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2025-05-25 19:34:42 -0600

    added grid guideline to dashboard

--- src/components/dashboard/profitability.tsx
+++ src/components/dashboard/profitability.tsx
@@ -2,13 +2,27 @@
 
 export function Profitability() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-      </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    <div className="w-full h-full grid grid-cols-3 p-4 gap-4">
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">01</div>
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">02</div>
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">03</div>
+
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">04</div>
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">05</div>
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">06</div>
+
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">07</div>
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">08</div>
+      <div className="bg-muted-foreground flex justify-center items-center rounded-xl text-white">09</div>
     </div>
   )
 }
+
+// <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+//   <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+//     <div className="aspect-video rounded-xl bg-muted/50" />
+//     <div className="aspect-video rounded-xl bg-muted/50" />
+//     <div className="aspect-video rounded-xl bg-muted/50" />
+//   </div>
+//   <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+// </div>
